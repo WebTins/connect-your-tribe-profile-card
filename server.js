8196725e54a47dc07a9fae0aa4e2b74fae965c8b
Tin Nguyen <powertinnguyen@gmail.com>
2026-02-04 12:41:00 +0100

    #4 Custom data gebruiken en elkaar helpen
    Custom velden opgehaald en getoont in de website met JSON.parse

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const personResponse = await fetch('https://fdnd.directus.app/items/person/' + p
 
 // Lees van de response van die fetch het JSON object in, waar we iets mee kunnen doen
 const personResponseJSON = await personResponse.json()
+const personData = personResponseJSON.data
+personData.custom = JSON.parse(personData.custom)
 
 // Controleer eventueel de data in je console
 // (Let op: dit is _niet_ de console van je browser, maar van NodeJS, in je terminal)
